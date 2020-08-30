df93a159694b0b911e0739a5c3f5709ce03647f7
Kita Fujita <kitafujita@gmail.com>
2020-08-30 14:55:03 -0500

    Added get state for leds

--- atv_hat_ctrl.py
+++ atv_hat_ctrl.py
@@ -26,17 +26,16 @@ fan speeds of a specified port, see json for temperature,
 fan speed, and network configuration
 
 Usage:
-	atv_hat_ctrl.py (-h | --help)
-    atv_hat_ctrl.py (-s=<new_state>|--state=<new_state>) ( (-p=<port_number>|--port=<port_number>) | (-l=<led_number>|--led=<led_number>))
-	atv_hat_ctrl.py (-g | --get)
-	atv_hat_ctrl.py (-i | --init)
+	atv_hat_ctrl.py -h
+	atv_hat_ctrl.py -g
+    atv_hat_ctrl.py -s=<new_state> ( -p=<port_number> | -l=<led_number>)
 
 Options:
-    -h --help
-    -g --get                               Get the state of LEDs and Ports
-    -p=<port_number> --port=<port_number>  Port number to toggle state of (0-47) [default: -1]
-    -l=<led_number>  --led=<led_number>    LED number to toggle state of (0-2) [default: -1]
-    -s=<new_state>   --state=<new_state>   State of port/led to set (0|1) (0==off 1==on) [default: 0]
+    -h    Print help text
+    -g    Get the state of LEDs and Ports
+    -p=<port_number> Port number to toggle state of (0-47) [default: -1]
+    -l=<led_number>  LED number to toggle state of (0-2) [default: -1]
+    -s=<new_state>   State of port/led to set (0|1) (0==off 1==on) [default: 0]
 
 """
 from docopt import docopt;
@@ -54,15 +53,26 @@ def setLedState(ledNum, newState):
 	GPIO.setup(ledGpioPin[ledNum], GPIO.OUT)
 	GPIO.output(ledGpioPin[ledNum],newState)
 
+def getState():
+	ledState = [];
+	for ledPin in ledGpioPin:
+		GPIO.setup(ledPin,GPIO.OUT); #need to set up every time
+		ledState.append(GPIO.input(ledPin));
+	return ledState;
+
 if __name__ == "__main__":
 	args = docopt(__doc__);
 
-	#pdb.set_trace()
 	#check state variable if used
 	if ((int(args["-s"])) != 0 and int(args["-s"]) != 1):
 		print("New State must be 0 or 1");
-		exit(-1);
+		exit(-1);		
 
 	#check for led updates
-	if (int(args["-l"]) >= 0 and int(args["-l"]) < 3):
+	if ((int(args["-l"]) >= 0 and int(args["-l"])) < 3):
 		setLedState(int(args["-l"]), int(args["-s"]));
+
+	#get state and print it
+	if (args["-g"]):
+		state = getState();
+		print(state);
